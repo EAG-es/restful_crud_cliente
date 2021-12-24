6ed94871016c6c4b580fee2600744ddb9aa5f607
informatica <informatica@informatica-VirtualBox>
2021-12-24 16:14:32 +0000

    Add files via upload

---
+++ src/main/java/inser/persistence/restful_crud/GamaProducto.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inser.persistence.restful_crud;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ *
+ * @author informatica
+ */
+// Si no tiene conexión con una base de datos, quitar:
+//@Entity
+//@Table(name = "gama_producto")
+//@XmlRootElement
+//@NamedQueries({
+//    @NamedQuery(name = "GamaProducto.findAll", query = "SELECT g FROM GamaProducto g"),
+//    @NamedQuery(name = "GamaProducto.findByGama", query = "SELECT g FROM GamaProducto g WHERE g.gama = :gama"),
+//    @NamedQuery(name = "GamaProducto.findByImagen", query = "SELECT g FROM GamaProducto g WHERE g.imagen = :imagen")})
+public class GamaProducto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "gama")
+    private String gama;
+    @Lob
+    @Column(name = "descripcion_texto")
+    private String descripcionTexto;
+    @Lob
+    @Column(name = "descripcion_html")
+    private String descripcionHtml;
+    @Column(name = "imagen")
+    private String imagen;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "gama")
+    private Collection<Producto> productoCollection;
+
+    public GamaProducto() {
+    }
+
+    public GamaProducto(String gama) {
+        this.gama = gama;
+    }
+
+    public String getGama() {
+        return gama;
+    }
+
+    public void setGama(String gama) {
+        this.gama = gama;
+    }
+
+    public String getDescripcionTexto() {
+        return descripcionTexto;
+    }
+
+    public void setDescripcionTexto(String descripcionTexto) {
+        this.descripcionTexto = descripcionTexto;
+    }
+
+    public String getDescripcionHtml() {
+        return descripcionHtml;
+    }
+
+    public void setDescripcionHtml(String descripcionHtml) {
+        this.descripcionHtml = descripcionHtml;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @XmlTransient
+    public Collection<Producto> getProductoCollection() {
+        return productoCollection;
+    }
+
+    public void setProductoCollection(Collection<Producto> productoCollection) {
+        this.productoCollection = productoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (gama != null ? gama.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof GamaProducto)) {
+            return false;
+        }
+        GamaProducto other = (GamaProducto) object;
+        if ((this.gama == null && other.gama != null) || (this.gama != null && !this.gama.equals(other.gama))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "inser.restful.restful_crud.GamaProducto[ gama=" + gama + " ]";
+    }
+    
+}
---
+++ src/main/java/inser/persistence/restful_crud/LinkedList_Producto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inser.persistence.restful_crud;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author informatica
+ */
+public class LinkedList_Producto implements Serializable {
+    public LinkedList<Producto> lista = null;
+    
+}
