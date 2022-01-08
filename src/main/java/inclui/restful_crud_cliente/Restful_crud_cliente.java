480b5071888520747324a2d9f49e3515b7998658
informatica <informatica@informatica-VirtualBox>
2022-01-08 17:38:25 +0000

    v20220108

--- src/main/java/inclui/restful_crud_cliente/Restful_crud_cliente.java
+++ src/main/java/inclui/restful_crud_cliente/Restful_crud_cliente.java
@@ -25,11 +25,12 @@ public class Restful_crud_cliente {
         String usuario = "jardineria";
         String contraseña = "2021jardineria";
         LinkedList_Producto linkedList_envuelta = new LinkedList_Producto();
-        try {            
-            Producto producto = new Producto ();
-//            producto_entidad = producto.find_JSON(inser.persistence.restful_crud.Producto.class
-//                    , "1", usuario, contraseña, error);
-//            ret = (producto_entidad != null);
+        try {
+//            Producto producto = new Producto ();
+            Producto producto = new Producto ("http://localhost:8080/webresources", "inser.restful.restful_crud.producto");
+            producto_entidad = producto.find_JSON(inser.persistence.restful_crud.Producto.class
+                    , "1", usuario, contraseña, error);
+            ret = (producto_entidad != null);
             if (ret) {
                 linkedList_envuelta = producto.findLike_descripcion_JSON(linkedList_envuelta.getClass()
                         , "0", "14", "%de%", usuario, contraseña, error);
@@ -51,21 +52,21 @@ public class Restful_crud_cliente {
 //            if (ret) {
 //                ret = producto.create_JSON(producto_entidad, usuario, contraseña, error);
 //            }
-            if (ret) {
-                filas_afectadas_num = producto.countREST(usuario, contraseña, error);
-                ret = (filas_afectadas_num != null);
-            }
-            if (ret) {
-                ret = producto.edit_JSON(producto_entidad, "1", usuario, contraseña, error);
-            }
-            if (ret) {
-                linkedList_envuelta = producto.findRange_JSON(linkedList_envuelta.getClass(), "1", "15", usuario, contraseña, error);
-                ret = (linkedList_envuelta != null);
-            }            
-            if (ret) {
-                linkedList_envuelta = producto.findAll_JSON(linkedList_envuelta.getClass(), usuario, contraseña, error);
-                ret = (linkedList_envuelta != null);
-            }            
+//            if (ret) {
+//                filas_afectadas_num = producto.countREST(usuario, contraseña, error);
+//                ret = (filas_afectadas_num != null);
+//            }
+//            if (ret) {
+//                ret = producto.edit_JSON(producto_entidad, "1", usuario, contraseña, error);
+//            }
+//            if (ret) {
+//                linkedList_envuelta = producto.findRange_JSON(linkedList_envuelta.getClass(), "1", "15", usuario, contraseña, error);
+//                ret = (linkedList_envuelta != null);
+//            }            
+//            if (ret) {
+//                linkedList_envuelta = producto.findAll_JSON(linkedList_envuelta.getClass(), usuario, contraseña, error);
+//                ret = (linkedList_envuelta != null);
+//            }            
         } catch (Exception e) {
             error [0] = e.getMessage();
             if (error[0] == null) {
